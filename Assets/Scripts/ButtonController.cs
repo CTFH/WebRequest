5dbc83afc85196d772089515ab56ce7b68617fd9
cf <hoge>
2022-02-19 12:43:01 +0900

    コメント追加

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -5,21 +5,21 @@ using UnityEngine.UI;
 using UnityEngine.Networking;
 public class ButtonController : MonoBehaviour
 {
-    public InputField et1;
+    public InputField et1;//インプットフィールド用の変数
     public InputField et2;
-    public Text result;
+    public Text result;　//結果表示用のテキスト
 
-    public void btClick()
+    public void btClick()//クリックしたときの処理 （ボタンのインスペクターから追加）
     {
-        string x = et1.text;
+        string x = et1.text;//入っている情報を.textで取得
         string y = et2.text;
-        StartCoroutine(HttpConnect(x, y));
+        StartCoroutine(HttpConnect(x, y));//通信で渡す　文字列で渡す
     }
     
     IEnumerator HttpConnect(string x, string y)
     {
-        WWWForm form = new WWWForm();
-        form.AddField("x", x);
+        WWWForm form = new WWWForm();//リクエスト通信
+        form.AddField("x", x);//情報追加
         form.AddField("y", y);
         string url = "https://joytas.net/php/calc.php";
         UnityWebRequest uwr = UnityWebRequest.Post(url, form);
@@ -30,6 +30,9 @@ public class ButtonController : MonoBehaviour
         }
         else
         {
+            //通信した結果をかきこむ
+            //通信した結果をdownloadHandker.textが持っている
+            //それをresult.textに代入
             result.text = uwr.downloadHandler.text;
         }
     }
